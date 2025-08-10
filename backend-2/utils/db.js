f9a0606396eb7764db154976de8f4395ef0436e3
sadhana <sadhana903545@gmail.com>
2025-08-10 17:39:32 +0530

    5 commit

--- backend-2/utils/db.js
+++ backend-2/utils/db.js
@@ -3,7 +3,7 @@ import "dotenv/config";
 
 export default function db(){
     mongoose
-      .connect(process.env.MONGODB_URL)
+      .connect(process.env.MONGODB_URI)
       .then(() => console.log("Database connected successfully.....😁"))
       .catch(() => console.error("Error while connecting the mongodb"));
 }
